5401df2b0e93de0bbe47c65f28bf6e156b662dd7
Emmanuel Yapura <eyapura96@gmail.com>
2023-06-06 00:38:50 -0300

    agrega funciones

--- src/servicios/getPeliculas.js
+++ src/servicios/getPeliculas.js
@@ -3,3 +3,15 @@ export async function getPeliculas (nroPagina) {
     let response = await peliculas.json();
     return response;
 }
+
+export async function getPeliculasTop() {
+    let peliculas = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=2a765e8f852998a076d69380c3d13494&language=en-US&page=1");
+    let response = await peliculas.json();
+    return response;
+}
+
+export async function getPeliculasUpcoming() {
+    let peliculas = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=2a765e8f852998a076d69380c3d13494&language=en-US&page=1");
+    let response = peliculas.json();
+    return response;
+}
