0774f89677793e35b24477c73ecb9b3a1f0208aa
Emmanuel Yapura <eyapura96@gmail.com>
2023-06-06 00:37:37 -0300

    modifica componente App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import CardPelicula from './componentes/CardPelicula'
 import Navbar from './componentes/Navbar'
-import Modal from './componentes/Modal';
+import HeroSlider from './componentes/HeroSlider';
+import { PeliculasUpcoming } from './componentes/PeliculasUpcoming';
 
 function App() {
 
   return (
     <div>
     <Navbar />
-    {/* Este componente muestra las 20 peliculas en una grilla de grid */}
+    <HeroSlider />
     <CardPelicula />
+    <PeliculasUpcoming />
     </div>
   );
 }
