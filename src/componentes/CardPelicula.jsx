339071bf169644fefc606a908547dc69d151fd2e
Emmanuel Yapura <eyapura96@gmail.com>
2023-06-08 18:02:10 -0300

    modifica estilos del componente CardPelicula

--- src/componentes/CardPelicula.jsx
+++ src/componentes/CardPelicula.jsx
@@ -30,9 +30,9 @@ export default function CardPelicula() {
   }
 
   return (
-    <div className="bg-slate-600">
+    <div className="bg-slate-800">
       <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-15 lg:max-w-7xl lg:px-8">
-        <p className="text-2xl text-slate-50 font-serif font-medium tracking-wide pb-1 md:text-3xl lg:text-4xl lg:py-3">Mis peliculas favoritas</p>
+        <p className="text-3xl text-slate-50 font-serif italic font-medium tracking-wide pb-1 md:text-4xl lg:text-5xl lg:py-3">Mis peliculas favoritas</p>
         <div className="mt-2 grid grid-cols-2 gap-x-6 gap-y-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
           {peliculas.results?.map((pelicula) => (
             <div key={pelicula.id} className="group relative" onClick={() => {
@@ -46,25 +46,16 @@ export default function CardPelicula() {
                   className="h-full w-full object-fill object-center lg:h-full lg:w-full"
                 />
               </div>
-              <div className="mt-4 flex justify-between">
+              <div className="mt-2 flex justify-between">
                 <div>
-                  <h3 className="text-sm text-slate-100">
-                    <p >
+                  <h3 className="text-sm text-slate-100 md:text-base md:pl-1">
+                    <p>
                       <span aria-hidden="true" className="absolute inset-0" />
-                      {pelicula.original_title}
-                    </p>
-                  </h3>
-                  <h3 className="text-sm text-slate-100">
-                    <p >
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {pelicula.vote_average}
+                      {pelicula.title}
                     </p>
                   </h3>
                 </div>
-                <div>
-                  <p className="text-sm font-medium text-slate-100">{pelicula.release_date}</p>
-                  <p className="text-sm font-medium text-slate-100 text-right pr-1">{pelicula.adult ? "+18" : "ATP"}</p>
-                </div>
+                  <p className="text-sm font-medium text-slate-100 text-right md:text-base pr-2">{pelicula.adult ? "+18" : "ATP"}</p>
               </div>
             </div>
           ))}
