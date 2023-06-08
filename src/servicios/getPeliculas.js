0e0ee8e1c1d46f4be41f9f2b3074db6be8840166
Emmanuel Yapura <eyapura96@gmail.com>
2023-06-08 17:38:24 -0300

    crea una sola funcion para realizar las peticiones

--- src/servicios/getPeliculas.js
+++ src/servicios/getPeliculas.js
@@ -1,17 +1,5 @@
-export async function getPeliculas (nroPagina) {
-    let peliculas = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=2a765e8f852998a076d69380c3d13494&language=en-US&page=${nroPagina}`);
+export async function getPeliculas2( url ){
+    let peliculas = await fetch(url);
     let response = await peliculas.json();
-    return response;
-}
-
-export async function getPeliculasTop() {
-    let peliculas = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=2a765e8f852998a076d69380c3d13494&language=en-US&page=1");
-    let response = await peliculas.json();
-    return response;
-}
-
-export async function getPeliculasUpcoming() {
-    let peliculas = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=2a765e8f852998a076d69380c3d13494&language=en-US&page=1");
-    let response = peliculas.json();
-    return response;
+    return response ;
 }
