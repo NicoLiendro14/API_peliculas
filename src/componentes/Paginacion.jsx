1d5d3809436d95865fb0c37cffbdac78cc868ce2
Emmanuel Yapura <eyapura96@gmail.com>
2023-06-11 17:44:35 -0300

    elimina comentario

--- src/componentes/Paginacion.jsx
+++ src/componentes/Paginacion.jsx
@@ -1,5 +1,4 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
-/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */
 
 export default function Paginacion({ paginaActual, totalResultados, cambiarPeliculas }) {
 
