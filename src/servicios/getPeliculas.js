bf3f5a2c8a3512185531df5c713a0ec1f40c0b60
Emmanuel Yapura <eyapura96@gmail.com>
2023-06-11 17:44:14 -0300

    agrega axios para realizar las peticiones

--- src/servicios/getPeliculas.js
+++ src/servicios/getPeliculas.js
@@ -1,5 +1,6 @@
-export async function getPeliculas2( url ){
-    let peliculas = await fetch(url);
-    let response = await peliculas.json();
-    return response ;
+import axios from "axios";
+
+export async function getPeliculas2(url){
+    let peliculas = await axios.get(url);
+    return peliculas.data;
 }
